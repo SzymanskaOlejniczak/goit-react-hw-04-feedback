272b5efefb72ba438cbe82e2e3e582b0a6c48f95
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-21 19:52:02 +0100

    Update FeedBackOptions.jsx

--- src/components/FeedBackOptions/FeedBackOptions.jsx
+++ src/components/FeedBackOptions/FeedBackOptions.jsx
@@ -2,24 +2,20 @@ import PropTypes from 'prop-types';
 import style from './FeedBackOptions.module.css';
 
 export const FeedBackOptions = ({ options, onLeaveFeedback }) => (
-  
-    <div>
-      {options.map(option => (
-      
-          <button
-            key={option}
-            type="button"
-            name={option}
-            onClick={onLeaveFeedback}
-            className={style.feed_back}
-          >
-            {option}
-          </button>
-        )
-      )}
-    </div>
-  );
-
+  <div>
+    {options.map(option => (
+      <button
+        key={option}
+        type="button"
+        name={option}
+        onClick={onLeaveFeedback}
+        className={style.feed_back}
+      >
+        {option}
+      </button>
+    ))}
+  </div>
+);
 
 FeedBackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string.isRequired),
